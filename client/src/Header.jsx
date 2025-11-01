768df599b4aa3cd7f7269b42cc4b23a49b386594
IkerVeiga <veiga60.iker@gmail.com>
2025-11-01 16:42:08 +0100

    Header pantalla login

---
+++ client/src/Header.jsx
@@ -0,0 +1,15 @@
+import './style/Header.css'
+import logo from './assets/images/tigre-transparente.png'
+
+function Header() {
+    return(
+        <div id='headerDiv'>
+            <div id='titleDiv'>
+                <img id='logo' src={logo} alt="logoApp" />
+                <h1 id='title'>METROPOLITANO HC</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
