a6a7ec3704a86db63aff8a8fd6537d0920b30dd0
IkerVeiga <veiga60.iker@gmail.com>
2025-12-22 14:09:12 +0100

    la tarjeta de jugador se ve algo mejor

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,11 +1,13 @@
+import '../style/PlayerCard.css'
+
 export default function PlayerCard({ player }) {
     return (
         <>
-            <div>
-                <div>
-                    <img src={`/players/${player.photo}`} alt={`${player.name} ${player.lastName1} ${player.lastName2}`} />
+            <div id="playerCardDiv">
+                <div id="playerPhotoDiv">
+                    <img id="playerPhoto" src={`/players/${player.photo}`} alt={`${player.name} ${player.lastName1} ${player.lastName2}`} />
                 </div>
-                <p>{player.name} {player.lastName1} {player.lastName2}</p>
+                <p id="playerName">{player.name} {player.lastName1} {player.lastName2}</p>
             </div>
         </>
     )
