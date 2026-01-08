da8c4f99b537caaa2f21ef7a8e0a1e7d8d48ce3d
IkerVeiga <veiga60.iker@gmail.com>
2026-01-08 14:19:09 +0100

    estadisticas de equipo visibles

--- client/src/views/Stats.jsx
+++ client/src/views/Stats.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react';
 import PlayerCard from '../components/PlayerCard';
 import { useNavigate } from 'react-router-dom';
+import logoMetropolitano from '../assets/images/logo-metropolitanohc-negro-transparente.png'
 
 export default function Stats() {
 
@@ -18,7 +19,6 @@ export default function Stats() {
         try {
             const response = await axios.get(`${SERVER_URL}/players`, { withCredentials: true });
             setPlayers(response.data);
-            console.log(response.data);
         } catch (error) {
             console.log('Error al cargar los jugadores: ', error);
         }
@@ -58,7 +58,77 @@ export default function Stats() {
                         )}
                     </TabPanel>
                     <TabPanel id='teamTab'>
-                        {teamStats.points}
+                        <div>
+                            <p>Puntos</p>
+                            <p>{(3 * teamStats.gamesWon) + teamStats.gamesTied + teamStats.bonusPoints}</p>
+                        </div>
+                        <div>
+                            <p>Partidos jugados</p>
+                            <p>{teamStats.gamesWon + teamStats.gamesLost + teamStats.gamesTied}</p>
+                        </div>
+                        <div>
+                            <p>Partidos ganados</p>
+                            <p>{teamStats.gamesWon}</p>
+                        </div>
+                        <div>
+                            <p>Partidos perdidos</p>
+                            <p>{teamStats.gamesLost}</p>
+                        </div>
+                        <div>
+                            <p>Partidos empatados</p>
+                            <p>{teamStats.gamesTied}</p>
+                        </div>
+                        <div>
+                            <p>Bonus</p>
+                            <p>{teamStats.bonusPoints}</p>
+                        </div>
+                        <div>
+                            <p>Goles a favor</p>
+                            <p>{teamStats.goalsFor}</p>
+                        </div>
+                        <div>
+                            <p>Goles en contra</p>
+                            <p>{teamStats.goalsAgainst}</p>
+                        </div>
+                        <div>
+                            <p>Goal average</p>
+                            <p>{teamStats.goalsFor - teamStats.goalsAgainst}</p>
+                        </div>
+                        <div>
+                            <p>Power Play %</p>
+                            <p>{teamStats.powerPlayPercentage?.toFixed(2)}</p>
+                        </div>
+                        <div>
+                            <p>Penalty Kill %</p>
+                            <p>{teamStats.penaltyKillPercentage?.toFixed(2)}</p>
+                        </div>
+                        <div>
+                            <p>1vs0 +-</p>
+                            <p>{teamStats.oneVsZero}</p>
+                        </div>
+                        <div>
+                            <p>1vs1 +-</p>
+                            <p>{teamStats.oneVsOne}</p>
+                        </div>
+                        <div>
+                            <p>2vs1 +-</p>
+                            <p>{teamStats.twoVsOne}</p>
+                        </div>
+                        <div>
+                            <p>2vs2 +-</p>
+                            <p>{teamStats.twoVsTwo}</p>
+                        </div>
+                        <div>
+                            <p>3vs1 +-</p>
+                            <p>{teamStats.threeVsOne}</p>
+                        </div>
+                        <div>
+                            <p>3vs2 +-</p>
+                            <p>{teamStats.threeVsTwo}</p>
+                        </div>
+                        <div>
+                            <img src={logoMetropolitano} alt="Escudo Metropolitano HC" />
+                        </div>
                     </TabPanel>
                 </Tabs>
             </div>
