fdebc2ba1f2967f7d315aa70baa4ab39d53c8231
IkerVeiga <veiga60.iker@gmail.com>
2026-01-13 15:20:27 +0100

    empezar pantalla de inicio

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,15 +1,18 @@
 import Header from '../components/Header.jsx'
 import axios from 'axios'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import '../style/Home.css'
 
 export default function Home() {
 
     const SERVER_URL = 'http://localhost:8081';
+    const [authenticatedUser, setAuthenticatedUser] = useState(null);
 
     const whoAmI = async () => {
         try {
             const response = await axios.get(`${SERVER_URL}/me`, { withCredentials: true });
             console.log(response.data);
+            setAuthenticatedUser(response.data);
         } catch (error) {
             console.log('Error al obtener la información del usuario: ', error);
         }
@@ -22,6 +25,16 @@ export default function Home() {
     return (
         <>
             <Header />
+            {(authenticatedUser != null) &&
+                <div id='homePageContentDiv'>
+                    <div id='welcomeDiv'>
+                        <div id='welcomeTextDiv'>
+                            <p id='welcomeText'>¡Bienvenido, {authenticatedUser.username}!</p>
+                        </div>
+
+                    </div>
+                </div>
+            }
         </>
     )
 }
