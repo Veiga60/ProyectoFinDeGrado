0ae3a0f550d7e1fc2ba92338d93e2e666acd556c
IkerVeiga <veiga60.iker@gmail.com>
2026-01-24 19:09:13 +0100

    marcador de empezar partido algo mas bonito

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -7,20 +7,22 @@ export default function ScoreBoard({ match, localTeamGoals, visitingTeamGoals })
                 <div id='localImageDiv' className='teamImageDiv'>
                     <img src={`/logos/${match?.localTeam.logo}`} alt={match?.localTeam.name} />
                 </div>
-                <div>
-                    <p>{match?.localTeam.name}</p>
+                <div id='localNameDiv'>
+                    <p id='localNameText'>{match?.localTeam.name}</p>
                 </div>
-                <div>
-                    <p>{localTeamGoals}</p>
+                <div id='localGoalsDiv'>
+                    <p id='localGoalsText'>{localTeamGoals}</p>
                 </div>
             </div>
-            <p> - </p>
+            <div>
+                <p id='separatorText'> - </p>
+            </div>
             <div id='visitingDiv'>
-                <div>
-                    <p>{visitingTeamGoals}</p>
+                <div id='visitingGoalsDiv'>
+                    <p id='visitingGoalsText'>{visitingTeamGoals}</p>
                 </div>
-                <div>
-                    <p>{match?.visitingTeam.name}</p>
+                <div id='visitingNameDiv'>
+                    <p id='visitingNameText'>{match?.visitingTeam.name}</p>
                 </div>
                 <div id='visitingImageDiv' className='teamImageDiv'>
                     <img src={`/logos/${match?.visitingTeam.logo}`} alt={match?.visitingTeam.name} />
