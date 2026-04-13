29db6532ed301723e478f619dcc4bab5c267a3de
IkerVeiga <veiga60.iker@gmail.com>
2026-04-13 13:52:28 +0200

    estilo en modal de faltas

--- client/src/components/PenaltiesModal.jsx
+++ client/src/components/PenaltiesModal.jsx
@@ -32,19 +32,80 @@ export default function PenaltiesModal({ teamPenalty, onClose, matchEvents, setM
                 (
                     <div id='penaltiesModalMainDiv'>
                         <div id='penaltiesMainDiv'>
+                            <div id='penaltiesTitleDiv'>
+                                <p id='penaltiesTitleText'>PENALIZACIÓN</p>
+                            </div>
                             <div id='penalties'>
                                 <div id='trippingDiv' className='penaltyDiv' onClick={() => setPenaltyType('ZANCADILLA')}>
-                                    <p id='trippingText' className='penaltyText'>ZANCADILLA</p>
+                                    <p id='trippingText' className='penaltyText'>ZC</p>
+                                </div>
+                                <div id='chargingDiv' className='penaltyDiv' onClick={() => setPenaltyType('CARGA')}>
+                                    <p id='chargingText' className='penaltyText'>CG</p>
+                                </div>
+                                <div id='roughingDiv' className='penaltyDiv' onClick={() => setPenaltyType('VIOLENCIA INNECESARIA')}>
+                                    <p id='roughingText' className='penaltyText'>VI</p>
+                                </div>
+                                <div id='unsportsmanlikeDiv' className='penaltyDiv' onClick={() => setPenaltyType('CONDUCTA ANTIDEPORTIVA')}>
+                                    <p id='unsportsmanlikeText' className='penaltyText'>CA</p>
+                                </div>
+                                <div id='hookingDiv' className='penaltyDiv' onClick={() => setPenaltyType('ENGANCHAR')}>
+                                    <p id='hookingText' className='penaltyText'>EG</p>
+                                </div>
+                                <div id='holdingDiv' className='penaltyDiv' onClick={() => setPenaltyType('AGARRAR')}>
+                                    <p id='holdingText' className='penaltyText'>AG</p>
+                                </div>
+                                <div id='elbowingDiv' className='penaltyDiv' onClick={() => setPenaltyType('CODAZO')}>
+                                    <p id='elbowingText' className='penaltyText'>CZ</p>
+                                </div>
+                                <div id='kneeingDiv' className='penaltyDiv' onClick={() => setPenaltyType('RODILLAZO')}>
+                                    <p id='kneeingText' className='penaltyText'>RZ</p>
                                 </div>
                                 <div id='crossCheckingDiv' className='penaltyDiv' onClick={() => setPenaltyType('CARGA CON STICK')}>
-                                    <p id='crossCheckingText' className='penaltyText'>CARGA CON STICK</p>
+                                    <p id='crossCheckingText' className='penaltyText'>CC</p>
+                                </div>
+                                <div id='slashingDiv' className='penaltyDiv' onClick={() => setPenaltyType('GOLPEAR CON EL STICK')}>
+                                    <p id='slashingText' className='penaltyText'>SL</p>
+                                </div>
+                                <div id='highStickingDiv' className='penaltyDiv' onClick={() => setPenaltyType('STICK ALTO')}>
+                                    <p id='highStickingText' className='penaltyText'>SA</p>
+                                </div>
+                                <div id='spearingDiv' className='penaltyDiv' onClick={() => setPenaltyType('GOLPEAR CON LA PUNTA DEL STICK')}>
+                                    <p id='spearingText' className='penaltyText'>SP</p>
+                                </div>
+                                <div id='butEndingDiv' className='penaltyDiv' onClick={() => setPenaltyType('GOLPEAR CON EL TACO DEL STICK')}>
+                                    <p id='butEndingText' className='penaltyText'>BE</p>
+                                </div>
+                                <div id='misconductDiv' className='penaltyDiv' onClick={() => setPenaltyType('MALA CONDUCTA')}>
+                                    <p id='misconductText' className='penaltyText'>MC</p>
+                                </div>
+                                <div id='gameMisconductDiv' className='penaltyDiv' onClick={() => setPenaltyType('MALA CONDUCTA EN EL JUEGO')}>
+                                    <p id='gameMisconductText' className='penaltyText'>MCJ</p>
+                                </div>
+                                <div id='matchPenaltyDiv' className='penaltyDiv' onClick={() => setPenaltyType('PENALIZACIÓN DE PARTIDO')}>
+                                    <p id='matchPenaltyText' className='penaltyText'>PP</p>
+                                </div>
+                                <div id='benchPenaltyDiv' className='penaltyDiv' onClick={() => setPenaltyType('PENALIZACIÓN DE BANQUILLO')}>
+                                    <p id='benchPenaltyText' className='penaltyText'>PB</p>
+                                </div>
+                                <div id='delayOfGameDiv' className='penaltyDiv' onClick={() => setPenaltyType('RETRASO EN EL JUEGO')}>
+                                    <p id='delayOfGameText' className='penaltyText'>RJ</p>
+                                </div>
+                                <div id='penaltyShotDiv' className='penaltyDiv' onClick={() => setPenaltyType('TIRO DE PENALTI')}>
+                                    <p id='penaltyShotText' className='penaltyText'>TP</p>
+                                </div>
+                                <div id='improperEquipmentDiv' className='penaltyDiv' onClick={() => setPenaltyType('EQUIPACIÓN INDEVIDA')}>
+                                    <p id='improperEquipmentText' className='penaltyText'>EI</p>
                                 </div>
                             </div>
-                            <input type='number' placeholder='Minutos' onChange={(e) => setPenaltyTime(e.target.value)} />
-                            <input type="number" placeholder='Minuto' id="matchMinute" onChange={(e) => { setMatchMinute(e.target.value.padStart(2, '0')) }} />
-                            <input type="number" placeholder='Segundo' id="matchSecond" onChange={(e) => { setMatchSecond(e.target.value.padStart(2, '0')) }} />
-                            <button onClick={() => (penaltyTime == 2 || penaltyTime == 5 || penaltyTime == 10) && (matchMinute <= 20 && matchSecond <= 59 && matchMinute >= 0 && matchSecond >= 0) && ([setPenalty(), onClose()])}>GUARDAR</button>
-                            <button onClick={() => onClose()}>CERRAR</button>
+                            <div id='penaltiesModalInputsDiv'>
+                                <input className='penaltiesModalInput' type='number' placeholder='Minutos' onChange={(e) => setPenaltyTime(e.target.value)} />
+                                <input className='penaltiesModalInput' type="number" placeholder='Minuto' id="matchMinute" onChange={(e) => { setMatchMinute(e.target.value.padStart(2, '0')) }} />
+                                <input className='penaltiesModalInput' type="number" placeholder='Segundo' id="matchSecond" onChange={(e) => { setMatchSecond(e.target.value.padStart(2, '0')) }} />
+                            </div>
+                            <div id='penaltiesModalButtonsDiv'>
+                                <button className='penaltiesModalButton' onClick={() => (penaltyTime == 2 || penaltyTime == 5 || penaltyTime == 10) && (matchMinute <= 20 && matchSecond <= 59 && matchMinute >= 0 && matchSecond >= 0) && ([setPenalty(), onClose()])}>GUARDAR</button>
+                                <button className='penaltiesModalButton' onClick={() => onClose()}>CERRAR</button>
+                            </div>
                         </div>
                     </div>
                 ) : (
