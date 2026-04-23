67da0c4c7c3abd800ecb156a92353c696f7aa9b4
IkerVeiga <veiga60.iker@gmail.com>
2026-04-23 18:39:44 +0200

    metodo para crear recomendacion de jugador

--- server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/facade/PlayerRecomendationController.java
+++ server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/facade/PlayerRecomendationController.java
@@ -1,8 +1,40 @@
 package com.ikerveiga.app.facade;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ikerveiga.app.dto.PlayerRecomendationDTO;
+import com.ikerveiga.app.service.PlayerRecomendationService;
 
 @RestController
 public class PlayerRecomendationController {
 
+    PlayerRecomendationService playerRecomendationService;
+
+    @Autowired
+    public PlayerRecomendationController(PlayerRecomendationService playerRecomendationService) {
+        this.playerRecomendationService = playerRecomendationService;
+    }
+
+    @PostMapping("/ai/recomendations/player")
+    public ResponseEntity<Void> createTeamRecomendation(@RequestBody PlayerRecomendationDTO playerRecomendationDTO) {
+        try {
+            playerRecomendationService.createPlayerRecomendation(playerRecomendationDTO.getArea(),
+                    playerRecomendationDTO.getDescription(), playerRecomendationDTO.getPlayer().getId(),
+                    playerRecomendationDTO.getMatch().getId());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (RuntimeException e) {
+            if (e.getMessage().equals("Match not found")) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else if (e.getMessage().equals("Player not found")) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+    }
 }
--- server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/service/PlayerRecomendationService.java
+++ server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/service/PlayerRecomendationService.java
@@ -1,8 +1,44 @@
 package com.ikerveiga.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ikerveiga.app.dao.MatchRepository;
+import com.ikerveiga.app.dao.PlayerRecomendationRepository;
+import com.ikerveiga.app.dao.PlayerRepository;
+import com.ikerveiga.app.entity.Match;
+import com.ikerveiga.app.entity.Player;
+import com.ikerveiga.app.entity.PlayerRecomendation;
 
 @Service
 public class PlayerRecomendationService {
 
+    PlayerRecomendationRepository playerRecomendationDAO;
+    MatchRepository matchDAO;
+    PlayerRepository playerDAO;
+
+    @Autowired
+    public PlayerRecomendationService(PlayerRecomendationRepository playerRecomendationDAO, MatchRepository matchDAO,
+            PlayerRepository playerDAO) {
+        this.playerRecomendationDAO = playerRecomendationDAO;
+        this.matchDAO = matchDAO;
+        this.playerDAO = playerDAO;
+    }
+
+    public void createPlayerRecomendation(String area, String description, long matchId, long playerId) {
+        Match match = matchDAO.findById(matchId);
+        Player player = playerDAO.findById(playerId);
+
+        if (match == null) {
+            throw new RuntimeException("Match not found");
+        }
+
+        if (player == null) {
+            throw new RuntimeException("Player not found");
+        }
+
+        PlayerRecomendation playerRecomendation = new PlayerRecomendation(area, description, player, match);
+
+        playerRecomendationDAO.save(playerRecomendation);
+    }
 }
