2761dcad8ab19c0b4c4aa81b414fbc11d1864307
IkerVeiga <veiga60.iker@gmail.com>
2026-07-06 16:18:18 +0200

    eliminar console.log de debug

--- client/src/components/ClubTeamSelector.jsx
+++ client/src/components/ClubTeamSelector.jsx
@@ -6,7 +6,6 @@ export default function ClubTeamSelector({ clubTeams, setClubTeamId }) {
             <div id='clubTeamSelectorMainDiv'>
                 {
                     clubTeams?.map((clubTeam, index) => {
-                        console.log(clubTeam.description);
                         return <div key={clubTeam.id} className={(index == 0) ? 'clubTeamSelectorDivFirst' : ((index == clubTeams.length - 1) ? 'clubTeamSelectorDivLast' : 'clubTeamSelectorDiv')} onClick={() => setClubTeamId(clubTeam.id)}><p className='clubTeamSelectorText'>{`${clubTeam.description}`}</p></div>
                     })
                 }
