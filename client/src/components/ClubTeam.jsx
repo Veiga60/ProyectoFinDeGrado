969d1abb68676ca7d148edaeb667a2c6555acfd0
IkerVeiga <veiga60.iker@gmail.com>
2026-08-22 11:46:24 +0200

    seleccionar y deseleccionar categorias entrenador

--- client/src/components/ClubTeam.jsx
+++ client/src/components/ClubTeam.jsx
@@ -1,15 +1,16 @@
 import '../style/ClubTeam.css'
 import logo from '../assets/images/tigre-transparente.png'
 
-export default function ClubTeam({ description }) {
+export default function ClubTeam({ description, isSelected, onClick }) {
+
     return (
         <>
-            <div className="clubTeamMainDiv">
+            <div className={(isSelected) ? ("clubTeamMainDivSelected") : ("clubTeamMainDiv")} onClick={onClick}>
                 <div className="clubTeamImageDiv">
                     <img src={logo} alt="Metropolitano HC" />
                 </div>
                 <div className='clubTeamDescDiv'>
-                    <p className='clubTeamDescText'>{description}</p>
+                    <p className={(isSelected) ? ('clubTeamDescTextSelected') : ('clubTeamDescText')}>{description}</p>
                 </div>
             </div>
         </>
--- client/src/views/SelectClubTeam.jsx
+++ client/src/views/SelectClubTeam.jsx
@@ -1,31 +1,36 @@
+import { useState } from 'react';
 import ClubTeam from "../components/ClubTeam";
 import Header from "../components/Header";
 import '../style/SelectClubTeam.css'
 
 export default function SelectClubTeam() {
+    const [selectedClubTeams, setSelectedClubTeams] = useState([]);
+
+    const toggleClubTeam = (teamName) => {
+        setSelectedClubTeams((prevSelectedClubTeams) => {
+            if (prevSelectedClubTeams.includes(teamName)) {
+                return prevSelectedClubTeams.filter((t) => t !== teamName);
+            } else {
+                return [...prevSelectedClubTeams, teamName];
+            }
+        });
+    };
+
+    const teams = ['ÉLITE', 'PLATA', 'JÚNIOR', 'JUVENIL', 'INFANTIL', 'ALEVÍN'];
+
     return (
         <>
             <Header />
             <div id="selectClubTeamMainDiv">
                 <div id="clubTeamsDiv">
-                    <ClubTeam
-                        description={'ÉLITE'}
-                    />
-                    <ClubTeam
-                        description={'PLATA'}
-                    />
-                    <ClubTeam
-                        description={'JÚNIOR'}
-                    />
-                    <ClubTeam
-                        description={'JUVENIL'}
-                    />
-                    <ClubTeam
-                        description={'INFANTIL'}
-                    />
-                    <ClubTeam
-                        description={'ALEVÍN'}
-                    />
+                    {teams.map((teamName) => (
+                        <ClubTeam
+                            key={teamName}
+                            description={teamName}
+                            isSelected={selectedClubTeams.includes(teamName)}
+                            onClick={() => toggleClubTeam(teamName)}
+                        />
+                    ))}
                 </div>
             </div>
         </>
