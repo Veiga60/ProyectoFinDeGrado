5aa966840be73a4ebbbdedd6a80e0da4bd26cdf0
IkerVeiga <veiga60.iker@gmail.com>
2026-09-23 10:58:18 +0200

    texto en recomendaciones de equipo

--- server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/service/TeamRecomendationService.java
+++ server/proyecto-fin-de-grado/src/main/java/com/ikerveiga/app/service/TeamRecomendationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ikerveiga.app.dao.MatchRepository;
 import com.ikerveiga.app.dao.TeamRecomendationRepository;
@@ -34,6 +35,7 @@ public class TeamRecomendationService {
         teamRecomendationDAO.save(teamRecomendation);
     }
 
+    @Transactional
     public List<TeamRecomendation> getTeamRecomendations(long matchId) {
         Match match = matchDAO.findById(matchId);
 
